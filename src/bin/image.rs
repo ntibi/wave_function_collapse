c6aa1b5fe4a05aeca56dfaf132baad60164aed71
ntibi <ntibi@protonmail.com>
2024-02-17 03:18:31 +0100

    functional overlapping model, the output is still not great tho

--- src/bin/image.rs
+++ src/bin/image.rs
@@ -3,6 +3,7 @@ use rand::{rngs, seq::SliceRandom, Rng, SeedableRng};
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     env,
+    num::Wrapping,
 };
 
 #[derive(Hash, Eq, PartialEq, Clone, Debug)]
@@ -14,9 +15,13 @@ impl Pattern {
     fn new(pattern: Vec<u32>) -> Self {
         Pattern { pattern }
     }
+
+    fn get_center(&self) -> u32 {
+        self.pattern[self.pattern.len() / 2]
+    }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct WeightedPattern {
     pattern: Pattern,
     weight: f32,
@@ -33,6 +38,9 @@ struct WfcGenerator {
     patterns: Vec<WeightedPattern>,
 }
 
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct PatternId(usize);
+
 impl WfcGenerator {
     /// Create a new WfcGenerator from an image
     /// * `range` - the range to look around to infer rules
@@ -42,7 +50,6 @@ impl WfcGenerator {
 
         let data = WfcGenerator::parse_data(&img, width as usize, height as usize);
         let patterns = WfcGenerator::infer_patterns(&data, range, width as usize, height as usize);
-        println!("patterns: {:?}", patterns);
 
         WfcGenerator { patterns, range }
     }
@@ -136,10 +143,217 @@ impl WfcGenerator {
 
         println!("found {} patterns", patterns.len());
 
+        let total = patterns.values().sum::<usize>() as f32;
         patterns
             .iter()
-            .map(|(p, &w)| WeightedPattern::new(p.clone(), w as f32))
+            .map(|(p, &w)| WeightedPattern::new(p.clone(), w as f32 / total))
             .collect()
+    }
+
+    /// do these pattern match in the given direction
+    fn check_overlap(&self, pattern: &Pattern, dir: usize, other: &Pattern) -> bool {
+        let other_relative_dir = (self.range * 2 + 1).pow(2) - dir - 1;
+        pattern.pattern[dir] == other.pattern[other_relative_dir]
+    }
+
+    fn get_pattern(&self, pattern_id: PatternId) -> &WeightedPattern {
+        &self.patterns[pattern_id.0]
+    }
+
+    fn is_allowed(
+        &self,
+        pattern: Pattern,
+        data: &Vec<Vec<(PatternId, f32)>>,
+        x: usize,
+        y: usize,
+        width: usize,
+        height: usize,
+    ) -> bool {
+        let neighbours = self.get_neighbours(x, y, width, height);
+        // for each neighbour of the input cell
+        for neighbour in neighbours.iter() {
+            let neighbour_patterns = &data[*neighbour];
+            // if the neighbour is not collapsed, we skip it
+            if neighbour_patterns.len() != 1 {
+                continue;
+            }
+
+            // TODO we could have a lookup table where we precompute
+            // all the possible patterns, for each pattern, for each direction
+            let neighbour_pattern = self.get_pattern(neighbour_patterns[0].0);
+            for neighbour_x in 0..(self.range * 2 + 1) {
+                for neighbour_y in 0..(self.range * 2 + 1) {
+                    let neighbour_index = neighbour_x + neighbour_y * (self.range * 2 + 1);
+                    // if the cell around the neighbour is not in the range of the input cell, we skip it
+                    // TODO we could do this just by modifying the double x,y loop, but im too lazy for now
+                    //   rn this is extremely inefficient
+                    if !neighbours.contains(&neighbour_index) {
+                        continue;
+                    }
+                    // dir relative to the input cell
+                    let x = Wrapping(x);
+                    let y = Wrapping(y);
+                    let neighbour_x = Wrapping(neighbour_x);
+                    let neighbour_y = Wrapping(neighbour_y);
+                    let size = Wrapping(self.range * 2 + 1);
+                    let half_pow = Wrapping((self.range * 2 + 1).pow(2) / 2);
+                    let dir = (neighbour_x - x + (neighbour_y - y) * (size) + (half_pow)).0;
+                    if !self.check_overlap(&pattern, dir, &neighbour_pattern.pattern) {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    fn get_weighted_possible_patterns(
+        &self,
+        data: &Vec<Vec<(PatternId, f32)>>,
+        x: usize,
+        y: usize,
+        width: usize,
+        height: usize,
+    ) -> Vec<(PatternId, f32)> {
+        // for each pattern id
+        let mut possible_patterns: Vec<(PatternId, f32)> = Vec::new();
+        for (pattern_id, _) in data[y * width + x].iter() {
+            let pattern = self.get_pattern(*pattern_id);
+            if self.is_allowed(pattern.pattern.clone(), data, x, y, width, height) {
+                possible_patterns.push((*pattern_id, 1.));
+            }
+        }
+
+        possible_patterns
+    }
+
+    fn get_neighbours(&self, x: usize, y: usize, width: usize, height: usize) -> Vec<usize> {
+        let mut neighbours = Vec::new();
+
+        for dir in 0..(self.range * 2 + 1).pow(2) {
+            // do not return self
+            if dir == self.range * (self.range * 2 + 1) + self.range {
+                continue;
+            }
+            let xx = dir % (self.range * 2 + 1);
+            let yy = dir / (self.range * 2 + 1);
+            if (x + xx).checked_sub(self.range).is_some()
+                && (y + yy).checked_sub(self.range).is_some()
+                && x + xx <= width
+                && y + yy <= height
+            {
+                let i = ((x + xx - self.range) + (y + yy - self.range) * width) as usize;
+                neighbours.push(i);
+            }
+        }
+
+        neighbours
+    }
+
+    fn gen(&self, width: usize, height: usize, seed: Option<u64>) -> Vec<u32> {
+        let seed = seed.unwrap_or_else(|| rand::thread_rng().gen());
+        println!("seed: {}", seed);
+        let mut rng = rngs::StdRng::seed_from_u64(seed);
+
+        let weighted_pattern_ids: Vec<(PatternId, f32)> = (0..self.patterns.len())
+            .map(|id| {
+                let pid = PatternId(id);
+                (pid, self.get_pattern(pid).weight)
+            })
+            .collect::<Vec<_>>();
+        let mut data = vec![weighted_pattern_ids.clone(); width * height];
+        let mut stack: VecDeque<usize> = VecDeque::new();
+
+        loop {
+            if let Some(to_observe) = stack.pop_front() {
+                let patterns = &data[to_observe];
+                // we are exploring a collapsed cell
+                if patterns.len() == 1 {
+                    continue;
+                }
+                let (x, y) = (to_observe % width, to_observe / width);
+
+                let new_patterns = self.get_weighted_possible_patterns(&data, x, y, width, height);
+                // if we had a change, we apply it and propagate
+                if new_patterns != *patterns {
+                    data[to_observe] = new_patterns;
+                    for neighbour in self.get_neighbours(x, y, width, height).iter() {
+                        // only push non collapsed neighbours
+                        if data[*neighbour].len() > 1 {
+                            stack.push_back(*neighbour);
+                        }
+                    }
+                }
+            } else {
+                let indexes_with_entropy: Vec<(usize, f32)> = data
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, weighted_patterns)| {
+                        if weighted_patterns.len() > 1 {
+                            Some((
+                                i,
+                                -weighted_patterns.iter().fold(
+                                    0.,
+                                    // shannon's entropy (if not, all the tiles have the same entropy, especially on simple samples)
+                                    |acc, (_, w)| {
+                                        // log2(0) is -inf, and 0 * -inf is nan
+                                        // so we just cut to -inf
+                                        if *w > 0. {
+                                            acc + w * w.log2()
+                                        } else {
+                                            acc + -f32::INFINITY
+                                        }
+                                    },
+                                ),
+                            ))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                // if we have no entropy, we are done
+                if indexes_with_entropy.is_empty() {
+                    return data
+                        .iter()
+                        .map(|s| match s.get(0) {
+                            Some(v) => self.get_pattern(v.0).pattern.get_center(),
+                            None => 0,
+                        })
+                        .collect::<Vec<u32>>();
+                }
+
+                let (_, lowest_entropy) = indexes_with_entropy
+                    .iter()
+                    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                    .unwrap();
+                let i = indexes_with_entropy
+                    .iter()
+                    .filter(|(_, entropy)| *entropy == *lowest_entropy)
+                    .collect::<Vec<_>>()
+                    .choose(&mut rng)
+                    .unwrap()
+                    .0;
+
+                match data[i].choose_weighted(&mut rng, |(_, weight)| *weight) {
+                    Ok((pattern, _)) => {
+                        data[i] = vec![(*pattern, 1.)];
+                    }
+                    Err(e) => {
+                        println!("failed to choose a pattern {}", e);
+                        data[i] = vec![];
+                    }
+                }
+
+                let (x, y) = (i % width, i / width);
+                for neighbour in self.get_neighbours(x, y, width, height).iter() {
+                    // only push non collapsed neighbours
+                    if data[*neighbour].len() > 1 {
+                        stack.push_back(*neighbour);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -154,23 +368,26 @@ fn main() {
     };
 
     let mut wfc = WfcGenerator::from_image(1, &img);
-    //wfc.debug_rules();
-    //let (w, h) = (3 as u32, 3 as u32);
-    //let start = std::time::Instant::now();
-    //let data = wfc.gen(w as usize, h as usize, None);
-    ////let data = wfc.gen(w as usize, h as usize, Some(17550241477713680911));
-    //println!("generated in {:?}", start.elapsed());
+    let (w, h) = (16 as u32, 16 as u32);
+    let start = std::time::Instant::now();
+    let data = wfc.gen(w as usize, h as usize, None);
+    println!("generated in {:?}", start.elapsed());
 
-    //let buffer: Vec<u8> = data
-    //.iter()
-    //.flat_map(|v| {
-    //v.to_be_bytes()
-    //.iter()
-    //.cloned()
-    //.skip(4 - wfc.bpp)
-    //.take(wfc.bpp)
-    //.collect::<Vec<_>>()
-    //})
-    //.collect();
-    //image::save_buffer("out.bmp", &buffer, w, h, wfc.format).unwrap()
+    let bpp = match img.color() {
+        ColorType::Rgb8 => 3,
+        ColorType::Rgba8 => 4,
+        _ => panic!("unsupported format {:?}", img.color()),
+    };
+    let buffer: Vec<u8> = data
+        .iter()
+        .flat_map(|v| {
+            v.to_be_bytes()
+                .iter()
+                .cloned()
+                .skip(4 - bpp)
+                .take(bpp)
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    image::save_buffer("out.bmp", &buffer, w, h, img.color()).unwrap()
 }
